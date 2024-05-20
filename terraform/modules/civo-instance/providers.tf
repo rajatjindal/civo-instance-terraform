b43f0b4663a67ddfc623659c7e11b029445e29a8
Uzair Ali <72073401+uzaxirr@users.noreply.github.com>
2024-05-20 15:16:11 +0530

    tweaked provider

--- terraform/modules/civo-instance/providers.tf
+++ terraform/modules/civo-instance/providers.tf
@@ -1,9 +1,11 @@
 terraform {
-  required_version = ">= 1.3.6"
-
   required_providers {
     civo = {
       source = "civo/civo"
     }
   }
+}
+provider "civo" {
+  token = var.civo_token
+  region = "FRA1"
 }
